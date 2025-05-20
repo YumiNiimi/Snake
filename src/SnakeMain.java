9ecf3a3acddb97291e0fcd9cfd527a2d43d73a5b
yumiko_niimi <yumiko_niimi@student.waylandps.org>
2025-05-20 15:08:04 -0400

    snake main

--- src/SnakeMain.java
+++ src/SnakeMain.java
@@ -111,7 +111,7 @@ public class SnakeMain extends JPanel{
         JFrame window = new JFrame("");
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        window.setBounds(0, 0, 400,  450 + 28); // title bar is 28 pixels!
+        window.setBounds(420, 0, 600,  650 + 28); // title bar is 28 pixels!
         JPanel panel = new JPanel();
 
         panel.setFocusable(true);
