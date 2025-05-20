8df55793d225580385aca2145bdeff9c70c71ce7
patara_tezduyar <patara_tezduyar@student.waylandps.org>
2025-05-20 14:26:50 -0400

    jd

---
+++ src/Apple.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+
+public class Apple {
+
+    public static final int SIZE = 25;
+
+    private boolean isApple;
+    private int row, col, numApples;
+
+    public Apple(int row, int col, boolean isApple){
+        this.row = row;
+        this.col = col;
+        this.isApple = isApple;
+    }
+
+    public void draw(Graphics2D g2){
+
+    }
+}
