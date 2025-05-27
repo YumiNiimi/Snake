f32ea5d86b9cafddd1e219270be18549155bdd42
yumiko_niimi <yumiko_niimi@student.waylandps.org>
2025-05-27 09:49:28 -0400

    snake main

--- src/Board.java
+++ src/Board.java
@@ -1,2 +1,21 @@
+import java.awt.*;
+
 public class Board {
+
+    private int w, h;
+
+    public Board(int w, int h){
+        this.w = w;
+        this.h = h;
+    }
+
+    public void draw(Graphics2D g2){
+
+        g2.setColor(new Color(98, 137, 64));
+        g2.fillRect(0, 0, 600, 650);
+        g2.setColor(new Color(84, 116, 54));
+        g2.fillRect(0, 0, 650, 70);
+        g2.setColor(new Color(179, 214, 101));
+        g2.fillRect(35, 95, w, h);
+    }
 }
--- src/SnakeMain.java
+++ src/SnakeMain.java
@@ -18,92 +18,97 @@ public class SnakeMain extends JPanel{
     private int lives;
     private int n;
 
+    private Board board;
+
 
 
     public SnakeMain(int w, int h){
-            setSize(w, h);
-            this.n = 0;
-            this.lives = 3;
+        setSize(w, h);
+        this.n = 0;
+        this.lives = 3;
 
+        board = new Board(530, 530);
 
-            snakeBody = new ArrayList<>();
+        snakeBody = new ArrayList<>();
 
             //how to add a new snake
             //sprites.add(new Turtle(Resources.turtle, new Point(410, 118), -3));
             //adding a snake
             //snake = new Sprite(Resources.snake, new Point(280, 568));
 
-            timer = new Timer(1000/60, e->update());
-            timer.start();
-            setupKeyListener();
-        }
+        timer = new Timer(1000/60, e->update());
+//        timer.start();
+        setupKeyListener();
+    }
 
         // called every frame (60 times per second by default) by the timer
-        public void update(){
+    public void update(){
             // update any time-based changes here
-            for(Sprite sprite: snakeBody) {
+        for(Sprite sprite: snakeBody) {
                 sprite.move();
-            }
-            boolean dead = false;
+        }
+        boolean dead = false;
 
             //if the snake hits itself
-            if(snake.intersects(snake)) {
-                dead = true;
-            }
+//        if(snake.intersects(snake)) {
+//            dead = true;
+//        }
 
             //if the snake hits the wall
-            if (snake.getX() < 5 || snake.getX() > 595) {
-                dead = true;
-                //snake.setImage(Resources.snake);
-                snake.setX(280);
-                snake.setY(568);
-            }
+//        if (snake.getX() < 5 || snake.getX() > 595) {
+//            dead = true;
+//            //snake.setImage(Resources.snake);
+//            snake.setX(280);
+//            snake.setY(568);
+//        }
 
-            repaint();
-        }
+        repaint();
+    }
 
         @Override
-        protected void paintComponent(Graphics g) {
-            super.paintComponent(g);
-            Graphics2D g2 = (Graphics2D)g;
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D)g;
             // all drawing happens here.
             // Best practice is to NOT change the state of any instance fields
             // so the graphics can update at any time
-            setBackground(Color.BLACK);
+        g2.setColor(Color.BLACK);
 
 
-            for(Sprite sprite : snakeBody)
-                sprite.draw(g2);
+        for(Sprite sprite : snakeBody)
+            sprite.draw(g2);
 
-            g2.setColor(Color.WHITE);
-            g2.setFont(new Font("Georgia", Font.PLAIN, 80));
+        g2.setColor(Color.WHITE);
+        g2.setFont(new Font("Georgia", Font.PLAIN, 80));
 
-            if(lives <= 0) {
-                g2.drawString("GAME OVER", 60, 320);
-            }
-            if(n == 5) {
-                g2.drawString("YOU WIN", 110, 300);
-            }
+        if(lives <= 0) {
+            g2.drawString("GAME OVER", 60, 320);
         }
+        if(n == 5) {
+            g2.drawString("YOU WIN", 110, 300);
+        }
+        board.draw(g2);
 
-        private boolean[] keys;
+    }
 
-        public void setupKeyListener(){
-            keys = new boolean[255];
-            addKeyListener(new KeyAdapter() {
-                @Override
-                public void keyPressed(KeyEvent e) {
-                    int key = e.getKeyCode();
-                    if(key == KeyEvent.VK_W || key == KeyEvent.VK_UP)
-                        snake.move(0, -1);
-                    if(key == KeyEvent.VK_S || key == KeyEvent.VK_DOWN)
-                        snake.move(0, 1);
-                    if(key == KeyEvent.VK_A || key == KeyEvent.VK_LEFT)
-                        snake.move(-1, 0);
-                    if(key == KeyEvent.VK_D || key == KeyEvent.VK_RIGHT)
-                        snake.move(1, 0);
-                }
-            });
+    private boolean[] keys;
+
+    public void setupKeyListener(){
+        keys = new boolean[255];
+        addKeyListener(new KeyAdapter() {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            int key = e.getKeyCode();
+            if(key == KeyEvent.VK_W || key == KeyEvent.VK_UP)
+                snake.move(0, -1);
+            if(key == KeyEvent.VK_S || key == KeyEvent.VK_DOWN)
+                snake.move(0, 1);
+            if(key == KeyEvent.VK_A || key == KeyEvent.VK_LEFT)
+                snake.move(-1, 0);
+            if(key == KeyEvent.VK_D || key == KeyEvent.VK_RIGHT)
+                snake.move(1, 0);
+        }
+        });
         }
 
 
@@ -112,7 +117,7 @@ public class SnakeMain extends JPanel{
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         window.setBounds(420, 0, 600,  650 + 28); // title bar is 28 pixels!
-        JPanel panel = new JPanel();
+        JPanel panel = new SnakeMain(600, 650);
 
         panel.setFocusable(true);
         panel.grabFocus();
